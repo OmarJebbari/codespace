19b104d9f2330e6bd39acfa34c3fe05df8d4dd5c
OmarJebbari <143851249+OmarJebbari@users.noreply.github.com>
2023-09-18 20:01:56 +0000

    Mon, Sep 18, 2023, 8:01 PM +00:00

--- m.c
+++ m.c
@@ -4,14 +4,13 @@
 
 int main (void)
 {
-    int i;
-    string = get_string("Enter a number : ");
+    char*c = get_string("Enter a number : ");
 
-    for (int i = 0 , i < 10 , i++)
+    for (int  i = 0 , i < 10 , i++)
     {
-        printf("%s = %s * %i",string , i);
+        printf("%c = %c * %i",c , i);
         string = string * i ;
     }
-
+   return 0;
 
 }
