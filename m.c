8d0af205944714c9a44f5a76acb1dfca152d8c21
OmarJebbari <143851249+OmarJebbari@users.noreply.github.com>
2023-09-18 20:11:19 +0000

    Mon, Sep 18, 2023, 8:11 PM +00:00

--- m.c
+++ m.c
@@ -7,11 +7,11 @@ int main (void)
   int array[] = {1,2,3,4,5,6,7,8,9,10};
   int sum = 0;
 
-  for ( int i = 0 , i < 8 , i++)
-  
+  for ( int i = 0 ; i < 10 ; i++)
    {
-
+     sum = array[i];
    }
+   printf("sum = %d",sum);
 
 
 }
